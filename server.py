75dd853c6bed3517f7237e22ae223800e2ed6e91
Shashwat Pathak <shashwat.pathak@facets.cloud>
2023-12-20 14:04:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,20 +2,25 @@ import grpc
 import greeter_pb2
 import greeter_pb2_grpc
 import concurrent.futures
+import logging
 
 class Greeter(greeter_pb2_grpc.GreeterServicer):
     def SayHello(self, request, context):
         response = greeter_pb2.HelloReply(message=f"Hello, {request.name}!")
-        print("Printing the request in console for us to see",request)
+        logging.info("Received request: %s", request)
         return response
 
 def serve():
+    logging.basicConfig(filename='grpc_server.log', level=logging.INFO)
+    
     server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
     greeter_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port("[::]:50051")
+    
+    logging.info("Starting server in [::]:50051")
+    
     server.start()
     server.wait_for_termination()
 
 if __name__ == "__main__":
-    print("Starting server in [::]:50051")
     serve()
